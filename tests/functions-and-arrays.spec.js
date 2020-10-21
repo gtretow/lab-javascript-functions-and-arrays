358e85d95eef19aac17b779dd9dbea011653c790
gtretow <guilherme.tretow@gmail.com>
2020-10-21 20:12:57 -0300

    number 2 fixed

--- tests/functions-and-arrays.spec.js
+++ tests/functions-and-arrays.spec.js
@@ -18,16 +18,16 @@ const words = [
   "orchard",
   "crackpot",
 ];
-let longest = "";
 
 function findLongestWord(words) {
+  let longest = words[0];
   if (words.length == 0) {
     return null;
   } else {
     for (i = 0; i < words.length; i++) {
       if (words[i].length > longest.length) {
         longest = words[i];
-      }
+      } 
     }
     return longest;
   }
@@ -198,7 +198,7 @@ function greatestProduct(numberUniq) {
   }
 }
 
-/// Iteration #8: Bonus
+// Iteration #8: Bonus
 
 const matrix = [
   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
